9eeab808ffad407a59621ca58d3e5db7b8ac22df
Thakur-Amit <atthakur0001@gmail.com>
2026-03-14 09:26:19 +0530

    le leo

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,7 +1,7 @@
 export default function sitemap() {
   return [
     {
-      url: 'https://www.boxyvault.vercel.app', // THAKUR: Yahan apna ASLI naya domain daaliye
+      url: 'https://boxyvault.vercel.app', // THAKUR: Yahan apna ASLI naya domain daaliye
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 1,
